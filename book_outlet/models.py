28f16e38e8f898ed47f671d8ab942317f1fc1596
bpourshojae <b.pourshojae@work.com>
2025-01-04 16:58:45 +0330

    Model file edited and save and update database and created modules

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -10,7 +10,8 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(11)]
         
     )
-    author = models.CharField(max_length=100)
+    author = models.CharField(null=True,max_length=100)
+    is_bestselling = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.title}({self.ratting})"
