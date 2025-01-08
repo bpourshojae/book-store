af782742fc12e9d6ddd170482bf696c46c081e5d
bpourshojae <b.pourshojae@work.com>
2025-01-08 10:35:21 +0330

    slug added in main branch

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -12,6 +13,11 @@ class Book(models.Model):
     )
     author = models.CharField(null=True,max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)
+    
+    def save(self,*args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title}({self.ratting})"
